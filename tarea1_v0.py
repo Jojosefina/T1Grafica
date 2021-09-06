ac7817063dcdeaf3daecd4f320d8fa34e1c855a7
Jojosefina <josefina.bustos.g@ug.uchile.cl>
2021-09-05 21:56:41 -0300

    Empezando

--- tarea1_v0.py
+++ tarea1_v0.py
@@ -1,7 +1,7 @@
 import glfw
 from OpenGL.GL import *
 import OpenGL.GL.shaders
-import numpy
+import numpy as np
 import sys
 
 __author__ = "Ivan Sipiran"
@@ -16,14 +16,43 @@ def crear_dama(x,y,r,g,b,radius):
     circle = []
     for angle in range(0,360,10):
         circle.extend([x, y, 0.0, r, g, b])
-        circle.extend([x+numpy.cos(numpy.radians(angle))*radius, 
-                       y+numpy.sin(numpy.radians(angle))*radius, 
+        circle.extend([x+np.cos(np.radians(angle))*radius, 
+                       y+np.sin(np.radians(angle))*radius, 
                        0.0, r, g, b])
-        circle.extend([x+numpy.cos(numpy.radians(angle+10))*radius, 
-                       y+numpy.sin(numpy.radians(angle+10))*radius, 
+        circle.extend([x+np.cos(np.radians(angle+10))*radius, 
+                       y+np.sin(np.radians(angle+10))*radius, 
                        0.0, r, g, b])
     
-    return numpy.array(circle, dtype = numpy.float32)
+    return np.array(circle, dtype = np.float32)
+
+def crear_tablero():
+
+    #Fondo blanco del tablero
+    Fondo = np.array([
+    #   positions        colors
+        -0.5, -0.5, 0.0,  1.0, 1.0, 1.0,
+         0.5, -0.5, 0.0,  1.0, 1.0, 1.0,
+         0.5,  0.5, 0.0,  1.0, 1.0, 1.0,
+        -0.5,  0.5, 0.0,  1.0, 1.0, 1.0
+    # It is important to use 32 bits data
+        ], dtype = np.float32)
+    
+    #Cuadraditos negros
+
+    CuadNeg = np.array([
+    #   positions        colors
+        -0.125, 0.375, 0.0,  0.0, 0.0, 0.0,
+         0.0, 0.375, 0.0,  0.0, 0.0, 0.0,
+         0.0,  0.5, 0.0,  0.0, 0.0, 0.0,
+        -0.125,  0.5, 0.0,  0.0, 0.0, 0.0
+    # It is important to use 32 bits data
+        ], dtype = np.float32)
+
+   # contador = 0 # Debe llegar a 32 (cuadrados negros)
+    #while contador != 32:
+         
+    return np.array()
+
 
 if __name__ == "__main__":
 
@@ -42,7 +71,9 @@ if __name__ == "__main__":
 
     glfw.make_context_current(window)
 
+    #########################################################
     dama = crear_dama(0.5,0.0, 0.0, 1.0, 0.0, 0.2)
+    tablero = crear_tablero()
 
     # Defining shaders for our pipeline
     vertex_shader = """
@@ -83,6 +114,7 @@ if __name__ == "__main__":
     glBindBuffer(GL_ARRAY_BUFFER, vboDama)
     glBufferData(GL_ARRAY_BUFFER, len(dama) * SIZE_IN_BYTES, dama, GL_STATIC_DRAW)
 
+
     # Telling OpenGL to use our shader program
     glUseProgram(shaderProgram)
 
@@ -99,9 +131,12 @@ if __name__ == "__main__":
     color = glGetAttribLocation(shaderProgram, "color")
     glVertexAttribPointer(color, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
     glEnableVertexAttribArray(color)
+
+    
     
     # It renders a scene using the active shader program (pipeline) and the active VAO (shapes)
     glDrawArrays(GL_TRIANGLES, 0, int(len(dama)/6))
+
 
     # Moving our draw to the active color buffer
     glfw.swap_buffers(window)
